a6546fc8fb7087da28f1955843743e2e1edb8710
Prajwal <sunnyborkar7777@gmail.com>
2024-01-20 13:06:24 +0530

    Added api

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SearchIcon from './search.svg';
 import MovieCard from "./MovieCard";
 import './App.css';
 
-const API_URL = 'http://www.omdbapi.com?apikey=b6003d8a';
+const API_URL = process.env.movie_api;
 
 const App = () => {
     const [searchTerm, setSearchTerm] = useState("");
